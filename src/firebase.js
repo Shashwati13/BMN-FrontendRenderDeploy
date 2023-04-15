09e65c6c67d40cf5abbffdd295f0db835f552f07
Shashwati Diware <shashwatidiware@gmail.com>
2023-04-14 20:11:45 -0400

    removed unnecessay file

--- src/firebase.js
+++ src/firebase.js
@@ -36,7 +36,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const googleProvider = new GoogleAuthProvider();
